29a41c419afd1af743dfb3c3a78f8c3a07ff5747
josephsanthoshk <josephsanthosh192@gmail.com>
2026-05-01 00:24:32 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,55 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # ------------------------
+    # ENCODE TEST
+    # ------------------------
 
-    # Wait for one clock cycle to see the output values
+    dut._log.info("Encode test")
+
+    data = 0b1011
+
+    dut.ui_in.value = data        # ui[3:0]
+    dut.ui_in.value |= 0 << 7     # MODE = 0
+
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    encoded = dut.uo_out.value & 0x7F
+    dut._log.info(f"Encoded word: {encoded}")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+
+
+    # ------------------------
+    # DECODE TEST (with error)
+    # ------------------------
+
+    dut._log.info("Decode test")
+
+    error_code = encoded ^ (1 << 2)  # flip bit 3
+
+    dut.ui_in.value = error_code
+    dut.ui_in.value |= 1 << 7        # MODE = 1
+
+    await ClockCycles(dut.clk, 1)
+
+    corrected = dut.uo_out.value & 0x7F
+    error_flag = (dut.uo_out.value >> 7) & 1
+
+    dut._log.info(f"Corrected code: {corrected}")
+
+    assert error_flag == 1
